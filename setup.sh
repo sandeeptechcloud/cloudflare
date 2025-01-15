d15b34244e875d02e2cc0ab53481c1bb3ded7201
sandeeptechcloud <97404767+sandeeptechcloud@users.noreply.github.com>
2025-01-16 01:06:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,58 @@
-z="
-";JEz='ps:/';CFz='chmo';kz='wk '\''';Kz='* ||';yz='e="n';rz='dmin';Dz='o)';VCz='3[31';XBz='e} >';Fz='[ ${';CBz='oppo';cBz='m,,}';VDz='/san';sCz=' you';mBz='${S2';SFz='--he';jCz='00m\';xBz='ell ';sDz='ed >';EEz='ce <';iBz='; th';Bz='(una';aFz='http';wz=' opp';YFz='pen-';uDz=' has';SDz='//gi';WFz='term';Mz='S^^}';EBz='="ye';cCz='ur i';fEz='VAR ';gz=' gre';wBz='r sh';hDz='| aw';UFz='for ';bFz='s://';Tz='PREF';oBz='S1}!';OEz='onte';xDz='dev/';ECz='0}\n';Hz='} ==';LFz='n co';nDz='= '\''v';dFz='agra';eDz='d --';nBz='}[${';ABz='o";;';XEz='s/he';az='net ';VFz='help';GCz='fi';hEz='REFI';WDz='deep';Qz='S'\''* ';uBz='prom';IDz='udfl';wCz='\n"';qz=' ${a';DDz='ull ';LCz='om >';uz='in';mEz='1"';TCz=';32m';LDz='v/nu';pz='case';Iz=' *'\''M';gBz='ite}';mCz='Sett';XCz='33[3';jz=' | a';DCz='e${R';VBz='ve:$';bCz='k yo';oDz='ersi';lDz='[ $v';uEz='\${a';fFz='m/sa';XFz='ux-o';KEz='/raw';Wz='"';Lz=' ${O';FEz='(cur';TFz='lp'\'' ';MBz='to r';TBz='or /';dBz=' != ';jDz='t $2';hBz='" ]]';BCz='ator';vCz='[00m';FCz=' 1';KFz='atio';Pz='NDOW';UBz='acti';oEz='2"';RFz='oufl';CCz=' mod';Jz='SYS'\''';TEz='chcl';dEz='cat ';ZCz='[34m';TDz='thub';nCz='ing-';NEz='serc';QEz='om/s';FBz='s";;';tz=',,} ';eBz='"${o';HEz='sSL ';sEz='bash';lBz='tf "';QDz='e ht';YDz='clou';mz='nt $';IEz='"htt';Ez='if [';qCz='flar';gDz='ion ';bz='user';EFz=' $PR';tBz='and ';yEz='{arg';cDz='ver=';tDz='if !';FFz='EFIX';lCz='[32m';KBz='ossi';Zz='m=$(';Yz='nPer';WBz='{opp';PCz=' ]; ';dz='inis';iCz='0;0;';KDz='>/de';yBz='in A';DEz='sour';RCz='[1;2';HBz='rint';pDz='on'\'' ';ODz='git ';qEz='3"';UEz='oud/';mDz='er !';NCz='" !=';xCz='cd $';EDz='2>&1';AEz=' 2>&';bBz='&1';hCz='033[';nEz='arg2';HFz='\n\n';gEz='> $P';BEz='1; t';dCz='nter';PDz='clon';HDz='/clo';VEz='test';NDz='>&1';NFz='ted ';UDz='.com';JBz='ot p';ZDz='d/cl';cEz='h")';cFz='inst';tEz=' clo';BBz='no) ';eEz='<<- ';MDz='ll 2';Nz=' == ';IFz='Conf';QFz=' '\''cl';FDz='rm -';YBz='/dev';aEz='/set';ez='trat';rDz='in/c';Az='OS=$';rBz='his ';vDz='h cl';JFz='igur';xEz='} \$';kEz='arg1';PEz='nt.c';JDz='are ';SBz='esac';OBz='his\';aCz='Chec';QBz='exit';QCz='\033';ADz='hare';qDz='IX/b';ZBz='/nul';GEz='l -f';yDz='null';CEz='hen';Rz=']]; ';rEz='are';eFz='m.co';DFz='d +x';gCz='on!\';fBz='ppos';gFz='ndee';oCz='up c';Sz='then';qBz='S4}R';iz='tive';BFz='VAR';bDz='lare';ICz=' -s ';ZFz='url ';REz='ande';aBz='l 2>';DBz='site';fCz='ecti';oz=')';wDz='d >/';WEz='/ref';JCz='goog';rCz='e in';UCz='[\03';sBz='comm';fz='or |';bEz='up.s';CDz='ev/n';vEz='rg1}';YCz='2m] ';pBz='}]${';vz='yes)';pCz='loud';kCz='n"';hz='p ac';Cz='me -';PBz='n"; ';Uz='IX="';iFz='ch';HCz='curl';lEz='="\$';LBz='ble ';PFz=' run';GFz='/bin';Gz='OS^^';SEz='epte';MCz=' "$?';vBz='pt o';kBz='prin';iEz='X/bi';RDz='tps:';lz='{pri';IBz='f "N';OCz=' '\''0'\''';iDz='k '\''{';GDz='rf $';jBz='en';SCz=';4;5';jEz='n/cl';GBz='*) p';eCz='conn';cz=' adm';Oz='*'\''WI';kDz='}'\'')';AFz='3}';WCz='m!\0';hFz='p_te';XDz='tech';Vz='/usr';pEz='arg3';LEz='.git';xz='osit';yCz='IX/s';tCz='r sy';MFz='mple';sz='Perm';RBz=' 1;;';dDz='$(cl';ZEz='main';KCz='le.c';aDz='oudf';NBz='un t';OFz='just';uCz='stem';fDz='vers';BDz=' >/d';nz='NF}'\''';wEz=' \${';YEz='ads/';MEz='hubu';ACz='istr';Xz='admi';
-eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$z$Tz$Uz$Vz$Wz$z$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$z$pz$qz$rz$sz$tz$uz$z$vz$wz$xz$yz$ABz$z$BBz$CBz$DBz$EBz$FBz$z$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$z$SBz$z$az$bz$cz$dz$ez$TBz$UBz$VBz$WBz$xz$XBz$YBz$ZBz$aBz$bBz$z$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$z$Ez$Fz$Xz$Yz$cBz$dBz$eBz$fBz$gBz$hBz$iBz$jBz$z$kBz$lBz$mBz$nBz$oBz$mBz$pBz$qBz$NBz$rBz$sBz$tBz$uBz$vBz$wBz$xBz$yBz$rz$ACz$BCz$CCz$DCz$ECz$Wz$z$QBz$FCz$z$GCz$z$GCz$z$HCz$ICz$JCz$KCz$LCz$YBz$ZBz$aBz$bBz$z$Ez$MCz$NCz$OCz$PCz$Sz$z$kBz$lBz$QCz$RCz$SCz$TCz$UCz$VCz$WCz$XCz$YCz$QCz$ZCz$aCz$bCz$cCz$dCz$az$eCz$fCz$gCz$hCz$iCz$iCz$jCz$kCz$z$QBz$FCz$z$GCz$z$kBz$lBz$QCz$lCz$mCz$nCz$oCz$pCz$qCz$rCz$sCz$tCz$uCz$QCz$vCz$wCz$z$xCz$Tz$yCz$ADz$BDz$CDz$DDz$EDz$z$FDz$GDz$Tz$yCz$ADz$HDz$IDz$JDz$KDz$LDz$MDz$NDz$z$ODz$PDz$QDz$RDz$SDz$TDz$UDz$VDz$WDz$XDz$YDz$ZDz$aDz$bDz$z$cDz$dDz$aDz$bDz$eDz$fDz$gDz$hDz$iDz$kBz$jDz$kDz$z$Ez$lDz$mDz$nDz$oDz$pDz$Rz$Sz$z$FDz$GDz$Tz$qDz$rDz$pCz$qCz$sDz$YBz$ZBz$aBz$bBz$z$GCz$z$tDz$uDz$vDz$aDz$bDz$wDz$xDz$yDz$AEz$BEz$CEz$z$DEz$EEz$FEz$GEz$HEz$IEz$JEz$KEz$LEz$MEz$NEz$OEz$PEz$QEz$REz$SEz$TEz$UEz$VEz$WEz$XEz$YEz$ZEz$aEz$bEz$cEz$z$GCz$z$dEz$eEz$fEz$gEz$hEz$iEz$jEz$aDz$bDz$z$kEz$lEz$mEz$z$nEz$lEz$oEz$z$pEz$lEz$qEz$z$xCz$Tz$yCz$ADz$HDz$IDz$rEz$z$sEz$tEz$IDz$JDz$uEz$vEz$wEz$nEz$xEz$yEz$AFz$z$BFz$z$CFz$DFz$EFz$FFz$GFz$HDz$IDz$rEz$z$kBz$lBz$HFz$IFz$JFz$KFz$LFz$MFz$NFz$OFz$PFz$QFz$RFz$JDz$SFz$TFz$UFz$VFz$HFz$Wz$z$WFz$XFz$YFz$ZFz$aFz$bFz$cFz$dFz$eFz$fFz$gFz$hFz$iFz"
+#!/bin/bash
+
+# Detecting OS and handling permission check only on Windows (MSYS/Windows environments)
+OS=$(uname -o)
+if [[ ${OS^^} == *'MSYS'* || ${OS^^} == *'WINDOWS'* ]]; then
+  PREFIX="/usr"
+  adminPerm=$(net user administrator | grep active | awk '{print $NF}')
+  case ${adminPerm,,} in
+    yes) opposite="no";;
+    no) opposite="yes";;
+    *) printf "Not possible to run this\n"; exit 1;;
+  esac
+  net user administrator /active:${opposite} >/dev/null 2>&1
+  adminPerm=$(net user administrator | grep active | awk '{print $NF}')
+  if [[ ${adminPerm,,} != "${opposite}" ]]; then
+    printf "\033[31m[!] Run this command prompt or shell in Administrator mode.\033[0m\n"
+    exit 1
+  fi
+fi
+
+# Checking internet connectivity
+curl -s google.com >/dev/null 2>&1
+if [ "$?" != '0' ]; then
+    printf "\033[1;31m[!] Check your internet connection!\033[0m\n"
+    exit 1
+fi
+
+# Setting up Cloudflare UI
+printf "\033[32mSetting up Cloudflare in your system...\033[0m\n"
+cd $PREFIX/share >/dev/null 2>&1
+rm -rf $PREFIX/share/cloudflare-ui >/dev/null 2>&1
+git clone https://github.com/BHUTUU/cloudflare-ui
+
+# Check if cloudflared is installed and if not, attempt to install
+ver=$(cloudflared --version 2>/dev/null | awk '{print $2}')
+if [[ $ver != 'version' ]]; then
+    rm -rf $PREFIX/bin/cloudflared >/dev/null 2>&1
+fi
+if ! hash cloudflared >/dev/null 2>&1; then
+    printf "\033[32mInstalling cloudflared...\033[0m\n"
+    source <(curl -fsSL "https://git.io/JinSa") || { echo "Cloudflared installation failed"; exit 1; }
+fi
+
+# Create a new cloudflare command in $PREFIX/bin
+cat <<- 'VAR' > $PREFIX/bin/cloudflare
+#!/bin/bash
+arg1="$1"
+arg2="$2"
+arg3="$3"
+cd $PREFIX/share/cloudflare-ui
+bash cloudflare ${arg1} ${arg2} ${arg3}
+VAR
+
+# Make it executable
+chmod +x $PREFIX/bin/cloudflare
+
+# Final message
+printf "\n\nConfiguration completed! Just run 'cloudflare --help' for help.\n\n"
